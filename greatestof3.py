def2b49b48042b291d60365194bfea5c78019f3c
Abdul Majid Khan <kmajid748@gmail.com>
2022-06-18 21:12:06 +0530

    Add files via upload

---
+++ greatestof3.py
@@ -0,0 +1,15 @@
+def maximum(a, b, c):
+  
+    if (a >= b) and (a >= c):
+        largest = a
+  
+    elif (b >= a) and (b >= c):
+        largest = b
+    else:
+        largest = c
+          
+    return largest
+a = 12
+b = 14
+c = 13
+print(maximum(a, b, c))
